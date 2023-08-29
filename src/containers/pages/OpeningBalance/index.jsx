9d3903ff3df73e3428be345816bd14fb56362a91
fighty91 <106462950+fighty91@users.noreply.github.com>
2023-08-29 09:20:03 +0900

    handling action button for opening balance

--- src/containers/pages/OpeningBalance/index.jsx
+++ src/containers/pages/OpeningBalance/index.jsx
@@ -12,14 +12,14 @@ const OpeningBalance = (props) => {
     const [accounts, setAccounts] = useState([])
     const [parentAccounts, setParentAccounts] = useState([])
     const [categories, setCategories] = useState([])
-    const [transactions, setTransactions] = useState([])
+    // const [transactions, setTransactions] = useState([])
+    const [openingBalance, setOpeningBalance] = useState({})
 
     const { getCurrencyAbs } = useGeneralFunc()
     
     const getDataAPI = () => {
         props.getAccountsFromAPI()
         props.getCategoriesFromAPI()
-        // props.getTransactionsFromAPI()
     }
 
     const setAccountsFromProps = () => {
@@ -33,10 +33,13 @@ const OpeningBalance = (props) => {
         let childAccounts = accounts.filter(e => e.parentId === accountId)
         childAccounts.forEach(acc => {
             let amount = 0
-            transactions.forEach((trans, i) => {
-                trans.transAccounts.forEach(e => {
-                    if(e.account === acc.id) amount = amount + e.debit - e.credit
-                })
+            // transactions.forEach((trans, i) => {
+            //     trans.transAccounts.forEach(e => {
+            //         if(e.account === acc.id) amount = amount + e.debit - e.credit
+            //     })
+            // })
+            openingBalance && openingBalance.transAccounts.forEach(e => {
+                if(e.account === acc.id) amount = amount + e.debit - e.credit
             })
             acc.amount = amount
             const category = categories.find(e => e.id === acc.categoryId)
@@ -58,14 +61,10 @@ const OpeningBalance = (props) => {
     }, [props.categories])
     
     useEffect(() => {
-        let temp = []
-        if(props.transactions.openingBalance) {
-            props.transactions.openingBalance.forEach(e => temp.push(e))
-        } else {
-            props.getOpeningBalanceFromAPI()
-        }
-        setTransactions(temp)
-        console.log(temp)
+        let temp
+        props.transactions.openingBalance ?
+        temp = props.transactions.openingBalance[0] : props.getOpeningBalanceFromAPI()
+        setOpeningBalance(temp)
     }, [props.transactions])
 
     let totalDebit = 0, totalCredit = 0
@@ -76,66 +75,85 @@ const OpeningBalance = (props) => {
                     <ContentHeader name="Opening Balance"/>
                     {/* Entry Content */}
                     <div className="mb-4">
-                        <Link to="create-opening-balance" className="btn btn-secondary btn-sm">Create Opening Balance</Link>
+                        {
+                            openingBalance ?
+                            <div className="dropdown">
+                                <button className="btn btn-sm btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Action
+                                </button>
+                                <ul className="dropdown-menu dropdown-menu-dark">
+                                    <li><Link className="dropdown-item" to="#">Update Opening Balance</Link></li>
+                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><Link className="dropdown-item" to="#">Clear Opening Balance</Link></li>
+                                </ul>
+                            </div>
+                            :
+                            <Link to="create-opening-balance" className="btn btn-secondary btn-sm">Create Opening Balance</Link>
+                        }
                     </div>
                     <div className="card pb-4">
                         <div className="card-body">
-                            <div className="table-responsive-sm">
-                                <table className="table table-striped table-sm table-transaction">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col" className="ps-2" colSpan={2}>Account</th>
-                                            <th scope="col" className="text-end">Debit</th>
-                                            <th scope="col" className="text-end pe-2">Credit</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className="table-group-divider">
-                                        {
-                                            parentAccounts.map(parentAccount => {
-                                                let { id, number, accountName } = parentAccount
-                                                const {childAccounts} = accountAmount(id)
-                                                return (
-                                                    <Fragment key={id}>
-                                                        <tr>
-                                                            <td className="fw-bold ps-2 pe-0 account-number"><Link to={`/accounts/account-detail/${id}?page=profile`} className="account-number ps-0 pe-0 ms-0 me-0">{ number }</Link></td>
-                                                            <td className="fw-bold ps-2"><Link to={`/accounts/account-detail/${id}?page=profile`} className="account-name pe-0 me-0">{ accountName }</Link></td>
-                                                            <td className="text-end fw-bold pe-2"></td>
-                                                            <td className="text-end fw-bold pe-2"></td>
-                                                        </tr>
-                                                        {
-                                                            childAccounts.map(acc => {
-                                                                const amount = acc.amount
-                                                                amount > 0 ? totalDebit += amount : totalCredit += amount
-                                                                return (
-                                                                    <tr key={acc.id}>
-                                                                        <td className="ps-2 pe-0 account-number"><Link to={`/accounts/account-detail/${acc.id}?page=profile`} className="account-number ps-0 pe-0 ms-0 me-0">{ acc.number }</Link></td>
-                                                                        <td className="ps-2"><Link to={`/accounts/account-detail/${acc.id}?page=profile`} className="account-name pe-0 me-0">{ acc.accountName }</Link></td>
-                                                                        <td className="text-end">
-                                                                            { getCurrencyAbs(amount > 0 ? amount : 0) }
-                                                                        </td>
-                                                                        <td className="text-end pe-2">
-                                                                            { getCurrencyAbs(amount < 0 ? amount : 0) }
-                                                                        </td>
-                                                                    </tr>
-                                                                )
-                                                            })
-                                                        }
-                                                    </Fragment>
-                                                )
-                                            })
-                                        }
-                                        <tr className="fw-bold">
-                                            <td className="ps-2 pe-2 text-secondary" colSpan={2}>Total Amount</td>
-                                            <td className="text-end text-primary">
-                                                { getCurrencyAbs(totalDebit) }
-                                            </td>
-                                            <td className="text-end pe-2 text-primary">
-                                                { getCurrencyAbs(totalCredit) }
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                            {
+                                openingBalance ?
+                                <div className="table-responsive-sm">
+                                    <table className="table table-striped table-sm table-transaction">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col" className="ps-2" colSpan={2}>Account</th>
+                                                <th scope="col" className="text-end">Debit</th>
+                                                <th scope="col" className="text-end pe-2">Credit</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="table-group-divider">
+                                            {
+                                                parentAccounts.map(parentAccount => {
+                                                    let { id, number, accountName } = parentAccount
+                                                    const {childAccounts} = accountAmount(id)
+                                                    return (
+                                                        <Fragment key={id}>
+                                                            <tr>
+                                                                <td className="fw-bold ps-2 pe-0 account-number"><Link to={`/accounts/account-detail/${id}?page=profile`} className="account-number ps-0 pe-0 ms-0 me-0">{ number }</Link></td>
+                                                                <td className="fw-bold ps-2"><Link to={`/accounts/account-detail/${id}?page=profile`} className="account-name pe-0 me-0">{ accountName }</Link></td>
+                                                                <td className="text-end fw-bold pe-2"></td>
+                                                                <td className="text-end fw-bold pe-2"></td>
+                                                            </tr>
+                                                            {
+                                                                childAccounts.map(acc => {
+                                                                    const amount = acc.amount
+                                                                    amount > 0 ? totalDebit += amount : totalCredit += amount
+                                                                    return (
+                                                                        <tr key={acc.id}>
+                                                                            <td className="ps-2 pe-0 account-number"><Link to={`/accounts/account-detail/${acc.id}?page=profile`} className="account-number ps-0 pe-0 ms-0 me-0">{ acc.number }</Link></td>
+                                                                            <td className="ps-2"><Link to={`/accounts/account-detail/${acc.id}?page=profile`} className="account-name pe-0 me-0">{ acc.accountName }</Link></td>
+                                                                            <td className="text-end">
+                                                                                { getCurrencyAbs(amount > 0 ? amount : 0) }
+                                                                            </td>
+                                                                            <td className="text-end pe-2">
+                                                                                { getCurrencyAbs(amount < 0 ? amount : 0) }
+                                                                            </td>
+                                                                        </tr>
+                                                                    )
+                                                                })
+                                                            }
+                                                        </Fragment>
+                                                    )
+                                                })
+                                            }
+                                            <tr className="fw-bold">
+                                                <td className="ps-2 pe-2 text-secondary" colSpan={2}>Total Amount</td>
+                                                <td className="text-end text-primary">
+                                                    { getCurrencyAbs(totalDebit) }
+                                                </td>
+                                                <td className="text-end pe-2 text-primary">
+                                                    { getCurrencyAbs(totalCredit) }
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                :
+                                <p>There are no transactions on the opening balance...</p>
+                            }
                         </div>
 
                     </div>
