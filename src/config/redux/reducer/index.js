d72870e8f757bc31f8b89d1359357b2a145ca874
fighty91 <106462950+fighty91@users.noreply.github.com>
2023-09-04 01:38:12 +0900

    handling param connection for create entries

--- src/config/redux/reducer/index.js
+++ src/config/redux/reducer/index.js
@@ -13,7 +13,7 @@ const initialState = {
     transactions: {},
     identicalCode: [],
 }
-  
+
 const reducer = (state = initialState, action)  => {
     const { type, value } = action
     switch (type) {
